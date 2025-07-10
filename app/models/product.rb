c8f388ae90a577d78d0ed3de7c39567c11d07600
Kevin Christ <kevin.christ@ionos.com>
2025-07-10 10:13:23 +0200

    adding a callback when a product gets created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   include Notifications
 
+  after_create :log_new_product
+
   has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
   has_rich_text :description
@@ -10,4 +12,9 @@ class Product < ApplicationRecord
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 
   after_update_commit :notify_subscribers, if: :back_in_stock?
+
+  private
+    def log_new_product
+      puts "A new product was added"
+    end
 end
